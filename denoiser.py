20f54f276025fd0d1345223987d6e8b84601975e
Nimish Karn <150058194+Nimish-Codes@users.noreply.github.com>
2023-12-25 22:08:08 +0545

    Update denoiser.py

--- denoiser.py
+++ denoiser.py
@@ -1,9 +1,20 @@
 import streamlit as st
 import numpy as np
+import matplotlib.pyplot as plt
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.losses import MeanSquaredError
+
+# Get the file path of the noisy image from the user
+noisy_image_path = st.text_input("Enter the full path of the noisy image on your local drive:")
+
+# Load and preprocess the noisy image
+noisy_img = image.load_img(noisy_image_path, target_size=(256, 256))
+noisy_img = image.img_to_array(noisy_img) / 255.0
+noisy_img = np.expand_dims(noisy_img, axis=0)
+
+# Print some information about the input image
+st.write("Noisy Image Shape:", noisy_img.shape)
 
 # Define the U-Net-like autoencoder model for denoising color images
 def denoising_model(input_shape):
@@ -26,34 +37,25 @@ def denoising_model(input_shape):
 # Create the model
 model = denoising_model(input_shape=(256, 256, 3))
 
-# Load pre-trained weights (if available)
-# model.load_weights("path_to_pretrained_weights.h5")
-
 # Compile the model with perceptual loss
 model.compile(optimizer='adam', loss='mse')  # You can also try MeanSquaredError()
 
-# Streamlit app
-st.title("Image Denoiser")
+# Print a summary of the model architecture
+st.write("Model Summary:")
+model.summary()
 
-# File uploader widget for uploading a noisy image
-noisy_image = st.file_uploader("Upload a Noisy Image", type=["image/*"])
+# Train the model
+model.fit(noisy_img, noisy_img, epochs=100, batch_size=1)
 
-if noisy_image is not None:
-    # Load and preprocess the noisy image
-    noisy_img = image.load_img(noisy_image, target_size=(256, 256))
-    noisy_img = image.img_to_array(noisy_img) / 255.0
-    noisy_img = np.expand_dims(noisy_img, axis=0)
+# Denoise the input noisy image
+denoised_img = model.predict(noisy_img)
 
-    # Print some information about the input image
-    st.image(noisy_img, caption="Noisy Image", use_column_width=True)
-    st.write("Noisy Image Shape:", noisy_img.shape)
+# Print some information about the denoised image
+st.write("Denoised Image Shape:", denoised_img.shape)
 
-    # Denoise the input noisy image
-    denoised_img = model.predict(noisy_img)
+# Plot original noisy and denoised images using Streamlit
+st.write("Noisy Image:")
+st.image(np.squeeze(noisy_img), caption='Noisy Image', use_column_width=True)
 
-    # Ensure pixel values are in the valid range (0 to 1)
-    denoised_img = np.clip(denoised_img, 0.0, 1.0)
-
-    # Print some information about the denoised image
-    st.image(denoised_img, caption="Denoised Image", use_column_width=True)
-    st.write("Denoised Image Shape:", denoised_img.shape)
+st.write("Denoised Image:")
+st.image(np.squeeze(denoised_img), caption='Denoised Image', use_column_width=True)
